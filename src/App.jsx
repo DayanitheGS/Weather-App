d3ab8d006cc4658d8bfeb09adfee5533533f1ea9
DayanitheGS <168344474+DayanitheGS@users.noreply.github.com>
2024-09-11 23:05:59 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+import Climate from './climate'
+
+
+
+function App() {
+ 
+  
+  
+ 
+ 
+
+  return (
+    <>
+   <Climate />
+      
+     
+
+    </>
+  )
+}
+
+export default App
